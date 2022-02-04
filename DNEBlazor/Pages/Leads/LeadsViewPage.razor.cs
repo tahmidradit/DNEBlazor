20d3862474eabefe0b3d4e00239c98140de8d07c
tahmidradit <tahmid.radit@outlook.com>
2022-02-04 18:51:38 +0600

    leads view page @code{} moved to another .cs file

--- DNEBlazor/Pages/Leads/LeadsViewPage.razor.cs
+++ DNEBlazor/Pages/Leads/LeadsViewPage.razor.cs
@@ -1,7 +1,10 @@
 ﻿using DNEBlazor.Data.Models;
 using DNEBlazor.Repository;
+using DNEBlazor.Service;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DNEBlazor.Pages.Leads
 {
@@ -10,8 +13,34 @@ namespace DNEBlazor.Pages.Leads
         [Inject]
         public ILead iLead { get; set; }
 
+        [Inject]
+        protected AuthenticationStateProvider authenticationStateProvider { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
+
         public Lead Lead = new Lead();
 
         public List<Lead> LeadsList => iLead.ToListLeads();
+        public void Remove(int Id)
+        {
+            string delete = iLead.Delete(Id);
+        }
+        public async Task Save()
+        {
+            if (Lead.Id == 0)
+            {
+                Lead = iLead.Add(Lead);
+            }
+            else
+            {
+                Lead = await iLead.Update(Lead, authenticationStateProvider);
+            }
+        }
+
+        //private void PreloadInputFields(Lead leadDM)
+        //{
+        //    Lead = leadDM;
+        //}
     }
 }
